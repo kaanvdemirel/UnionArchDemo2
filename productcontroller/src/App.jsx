08f555ce81df669eeacf69bf594f39fed21f9bad
kaan.demirel <kaandemirel190314@gmail.com>
2024-09-19 15:45:23 +0300

    Old React project removed. New  React project added.

---
+++ productcontroller/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom'; // Route'ý burada içe aktarýn
+import OverviewPage from './pages/OverviewPage';
+import ProductsPage from './pages/ProductsPage';
+
+function App() {
+    return (
+        <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+            <Routes>
+                <Route path='/' element={<OverviewPage />} />
+                <Route path='/products' element={<ProductsPage />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
